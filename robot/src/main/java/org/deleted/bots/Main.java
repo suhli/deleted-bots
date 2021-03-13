e3162b515f41300f5cd76705ce55b53eeb3338fb
shuhang <admin@shuhangli.com>
2021-03-14 00:42:01 +0800

    rm rebot

--- robot/src/main/java/org/deleted/bots/Main.java
+++
@@ -1,9 +0,0 @@
-package org.deleted.bots;
-
-import org.deleted.bots.init.Init;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new Init().init();
-    }
-}
